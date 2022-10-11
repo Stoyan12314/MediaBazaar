90a0cc4c607b0b93465e0c495ca2c7512fae5579
Mariyana Mileva <mariyana.mileva2@gmail.com>
2022-10-11 13:42:02 +0200

    Fixed errors

--- MediaBazaarWeb/WinFormsApp1/Program.cs
+++ MediaBazaarWeb/WinFormsApp1/Program.cs
@@ -17,7 +17,7 @@ namespace WinFormsApp1
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new EmployeeManagment());
+            Application.Run(new LoginForm());
         }
     }
 }
